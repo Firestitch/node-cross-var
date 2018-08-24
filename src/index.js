d3c1f83b9e0b3f1b20858322e2f9c5c908b22bb7
raygig <ray@firestitch.com>
2018-08-24 12:18:06 -0400

    Update index.js
    Added support for boolean config values and echoed out the actual command

--- src/index.js
+++ src/index.js
@@ -6,20 +6,25 @@ import { exec } from "child_process";
 import exit from 'exit';
 
 function normalize( args, isWindows ) {
+
     return args.map( arg => {
         Object.keys( process.env )
             .sort( ( x, y ) => x.length < y.length ) // sort by descending length to prevent partial replacement
             .forEach( key => {
                 const regex = new RegExp( `\\$${ key }|%${ key }%`, "ig" );
-                arg = arg.replace( regex, process.env[ key ] );
+                // remove single quotes for boolean value escaping
+                const value = String(process.env[ key ]).replace(/(^'|'$)/,'');
+                arg = arg.replace( regex,  value );
             } );
         return arg;
     } )
 }
 
 let args = process.argv.slice( 2 );
+
 if ( args.length === 1 ) {
     const [ command ] = normalize( args );
+    console.log('\x1b[36m%s\x1b[0m',command);
     const proc = exec( command, ( error, stdout, stderr ) => {
         if ( error ) {
             console.error( `exec error: ${ error }` );
@@ -30,8 +35,10 @@ if ( args.length === 1 ) {
         exit(proc.code);
     });
 } else {
+
     args = normalize( args );
     const command = args.shift();
+    console.log('\x1b[36m%s\x1b[0m',command + ' ' + args.join(' '));
     const proc = spawn.sync( command, args, { stdio: "inherit" } );
     exit(proc.status);
 }
